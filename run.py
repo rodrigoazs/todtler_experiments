fabc7676402901af891d32cf32020b062b82db53
rodrigoazs <rodrigo4zevedo@gmail.com>
2019-01-30 00:12:07 -0200

    Changed maxAtoms and maxClauses from 10000 to 1000

--- run.py
+++ run.py
@@ -28,8 +28,8 @@ def score(source, n_folds):
             'for inputfile in `find $1 -type f -name "*.mln" ! -name "*output*" ! -empty`; do',
             " logfile=`echo ${inputfile} | sed 's/.mln/-output.log/g'`;",
             " outputfile=`echo ${inputfile} | sed 's/.mln/-output.mln/g'`;",
-            ' echo "learnstruct -i ${inputfile} -o ${outputfile} -t ${databases} -search 999 -penalty 0.001 -minWt 0.05 -fractAtoms 1 -maxAtomSamples 10000 -maxClauseSamples 10000 -tightConvThresh 1e-3 -looseConvThresh 1e-2 -tightMaxIter 10 -plusType 2 -multipleDatabases -startFromEmptyMLN > ${logfile}";',
-            ' ./alchemy/bin/learnstruct -i ${inputfile} -o ${outputfile} -t ${databases} -search 999 -penalty 0.001 -minWt 0.05 -fractAtoms 1 -maxAtomSamples 10000 -maxClauseSamples 10000 -tightConvThresh 1e-3 -looseConvThresh 1e-2 -tightMaxIter 10 -plusType 2 -multipleDatabases -startFromEmptyMLN > ${logfile}',
+            ' echo "learnstruct -i ${inputfile} -o ${outputfile} -t ${databases} -search 999 -penalty 0.001 -minWt 0.05 -fractAtoms 1 -maxAtomSamples 1000 -maxClauseSamples 1000 -tightConvThresh 1e-3 -looseConvThresh 1e-2 -tightMaxIter 10 -plusType 2 -multipleDatabases -startFromEmptyMLN > ${logfile}";',
+            ' ./alchemy/bin/learnstruct -i ${inputfile} -o ${outputfile} -t ${databases} -search 999 -penalty 0.001 -minWt 0.05 -fractAtoms 1 -maxAtomSamples 1000 -maxClauseSamples 1000 -tightConvThresh 1e-3 -looseConvThresh 1e-2 -tightMaxIter 10 -plusType 2 -multipleDatabases -startFromEmptyMLN > ${logfile}',
             'done']
     return text
 
