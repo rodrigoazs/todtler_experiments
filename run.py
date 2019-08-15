e05d4a707940d0ad5da3e8e1458c901613a57404
rodrigoazs <rodrigo4zevedo@gmail.com>
2019-08-14 21:20:16 -0300

    Sent experiments for webkb

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ if os.name == 'posix' and sys.version_info[0] < 3:
     import subprocess32 as subprocess
 else:
     import subprocess
-    
+
 def score(source, n_folds):
     folds = ['domains/' + source + '-fold' + str(i+1) + '.db' for i in range(n_folds)]
     text = ['#!/bin/bash',
@@ -40,18 +40,18 @@ def call_process(call):
         os.waitpid(p.pid, 0)
     except:
         raise(Exception('Encountered problems while running process: ', call))
-        
+
 def write_to_file(content, path):
     '''Takes a list (content) and a path/file (path) and writes each line of the list to the file location.'''
     with open(path, 'w') as f:
         for line in content:
             f.write(line + '\n')
     f.close()
-    
+
 def save(data):
     with open('experiments/transfer_experiment.json', 'w') as fp:
         json.dump(data, fp)
-        
+
 def delete_train_files():
     '''Remove files from train folder'''
     try:
@@ -63,20 +63,20 @@ def delete_train_files():
         delete_files('csv')
     except:
         pass
-    
+
 def delete_models():
     try:
         delete_create_dir('models')
     except:
         pass
-    
+
 def delete_create_dir(path):
     try:
         shutil.rmtree(path)
     except:
         pass
     os.makedirs(path)
-    
+
 def save_experiment(data, experiment_title, exp_number):
     if not os.path.exists('experiments/' + experiment_title + '/experiment' + str(exp_number)):
         os.makedirs('experiments/' + experiment_title + '/experiment' + str(exp_number))
@@ -87,22 +87,22 @@ def save_experiment(data, experiment_title, exp_number):
     results.append(data)
     with open('experiments/' + experiment_title + '/experiment' + str(exp_number) + '/' + experiment_title + '.json', 'w') as fp:
         json.dump(results, fp)
-    
+
 def count_directories(d):
     return len([os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))])
-    
+
 def create_dir(path):
     try:
         os.makedirs(path)
     except:
         pass
-    
+
 def delete_files(ext):
     test = os.listdir()
     for item in test:
         if item.endswith('.' + ext):
             os.remove(item)
-            
+
 def get_last_model(path):
     test = os.listdir(path)
     last = 0
@@ -118,44 +118,44 @@ def get_last_model(path):
             #os.remove(path + '/' + item)
     #print('last is '+ str(last))
     return last
-    
+
 def remove_all_but(path, last):
     test = os.listdir(path)
     for item in test:
         if item != 'model-' + str(last) + '.mln':
             #print(path + '/' + item)
             os.remove(path + '/' + item)
-    
+
 def copy_all_models(path, experiment_path):
     test = os.listdir(path)
     for item in test:
         if item.startswith('model-') and item.endswith('.mln'):
             #print('copying '+ item)
-            shutil.copyfile(path + '/' + item, experiment_path + '/' + item)            
+            shutil.copyfile(path + '/' + item, experiment_path + '/' + item)
 
 def gen_train_files(source, datas):
     count = 1
     for data in datas:
         write_to_file(data, 'domains/' + source + '-fold' + str(count) + '.db')
         count += 1
-        
+
 def gen_train_file(source, data):
     try:
         os.makedirs('domains')
     except:
         pass
     write_to_file(data, 'domains/' + source + '.db')
-        
+
 def gen_source_bk(source):
     global bk
     write_to_file(bk[source], 'domains/' + source + '.mln')
-    
+
 def do_experiment(identifier, source, target, src_predicate, predicate):
     global bk
-    
+
     json = {}
     delete_train_files()
-    
+
     if source not in ['nell_sports', 'nell_finances', 'yago2s']:
         #[tar_train_facts, tar_test_facts] =  datasets.get_kfold_small(i, tar_data[0])
         #[tar_train_pos, tar_test_pos] =  datasets.get_kfold_small(i, tar_data[1])
@@ -169,10 +169,10 @@ def do_experiment(identifier, source, target, src_predicate, predicate):
         #[tar_train_pos, tar_test_pos] =  datasets.get_kfold_small(i, to_folds_pos)
         #[tar_train_neg, tar_test_neg] =  datasets.get_kfold_small(i, to_folds_neg)
         source_data[0] = [src_train_facts + to_folds_pos[0], src_train_facts + to_folds_pos[1], src_train_facts + to_folds_pos[2]]
-    
+
     #source_data = datasets.load(source)
     source_data = source_data[0]
-    
+
     if target not in ['nell_sports', 'nell_finances', 'yago2s']:
         #[tar_train_facts, tar_test_facts] =  datasets.get_kfold_small(i, tar_data[0])
         #[tar_train_pos, tar_test_pos] =  datasets.get_kfold_small(i, tar_data[1])
@@ -186,42 +186,42 @@ def do_experiment(identifier, source, target, src_predicate, predicate):
         #[tar_train_pos, tar_test_pos] =  datasets.get_kfold_small(i, to_folds_pos)
         #[tar_train_neg, tar_test_neg] =  datasets.get_kfold_small(i, to_folds_neg)
         target_data[0] = [tar_train_facts + to_folds_pos[0], tar_train_facts + to_folds_pos[1], tar_train_facts + to_folds_pos[2]]
-    
+
     #target_data = datasets.load(target)
     #target_data = target_data[0]
     target_data = target_data[0]
-    
+
     experiment_title = identifier + '_' + source + '_' + target
     create_dir('experiments/' + experiment_title)
     count_dir = count_directories('experiments/' + experiment_title)
     if count_dir < len(target_data): # n x n fold (target)
         start = time.time()
         exp_number = count_dir + 1
-        
+
         print('Doing experiment number ' + str(exp_number))
-        
-        gen_source_bk(source) 
+
+        gen_source_bk(source)
         gen_train_files(source, source_data)
 
         print('Generating templates for source domain')
-        templating = time.time()        
+        templating = time.time()
         CALL = '(java -jar todtler-generator.jar -maxLiterals 3 -maxVariables 3 -domainFile domains/' + source + '.mln -templateFile ' + source + '-templates.csv -formulaFile ' + source + '-formulas.csv -formulaDirectory clauses/' + source + ' > todtler-generator.txt 2>&1)'
         call_process(CALL)
         json['Generating source template time'] = time.time() - templating
         print('Time taken: %s' % json['Generating source template time'])
-        
+
         write_to_file(score(source, len(source_data)), 'score.sh')
-        
-        gen_source_bk(target)  
+
+        gen_source_bk(target)
         gen_train_files(target, target_data)
-        
+
         print('Generating templates for target domain')
-        templating = time.time()  
+        templating = time.time()
         CALL = '(java -jar todtler-generator.jar -maxLiterals 3 -maxVariables 3 -domainFile domains/' + target + '.mln -templateFile ' + target + '-templates.csv -formulaFile ' + target + '-formulas.csv -formulaDirectory clauses/' + target + ' > todtler-generator.txt 2>&1)'
         call_process(CALL)
         json['Generating target template time'] = time.time() - templating
         print('Time taken: %s' % json['Generating target template time'])
-        
+
         # sample clauses generated
         #clauses_size = 0.01
         #for clauses_path in ['clauses/' + source, 'clauses/' + target]:
@@ -230,35 +230,35 @@ def do_experiment(identifier, source, target, src_predicate, predicate):
         #    for file in onlyfiles[int(clauses_size * len(onlyfiles)):]:
         #        os.remove(os.path.join(clauses_path, file))
         #raise(Exception('aaa'))
-        
+
         print('Scoring clauses')
         scoring = time.time()
         CALL = '(./score.sh > score.txt 2>&1)'
         call_process(CALL)
         scoring = time.time() - scoring
         print('Time taken: %s' % scoring)
-        
+
         #raise(Exception('Scored clauses'))
-        
+
         json['Learning time'] = []
-        
+
         for i in range(len(target_data)):
             #delete_models()
             experiment_path = 'experiments/' + experiment_title + '/experiment' + str(exp_number) + '/fold' + str(i+1)
             create_dir(experiment_path)
-            
+
             print('Learning fold ' + str(i+1))
             print(predicate)
-            
+
             learning = time.time()
             CALL = '(java -jar todtler-learner.jar -sourceDirectory clauses/' + source + ' -targetDirectory clauses/' + target + ' -templateFileSource ' + source + '-templates.csv -formulaFileSource ' + source + '-formulas.csv -templateFileTarget ' + target + '-templates.csv -formulaFileTarget ' + target + '-formulas.csv -outputDirectory ' + experiment_path + ' -domainFile domains/' + target + '.mln -train domains/' + target + '-fold' + str(i+1) + '.db -ne ' + predicate.capitalize() + ' > todtler-learner.txt 2>&1)'
             call_process(CALL)
             learning = time.time() - learning
             json['Learning time'].append(learning)
             print('Time taken: %s' % learning)
-            
+
             #raise(Exception('ue'))
-            
+
             last = get_last_model(experiment_path)
             remove_all_but(experiment_path, last)
             #print('copying')
@@ -274,7 +274,7 @@ def do_experiment(identifier, source, target, src_predicate, predicate):
         print('Total experiment time taken %s' % json['Total experiment time'])
         save_experiment(json, experiment_title, exp_number)
     delete_train_files()
-    
+
 bk = {
       'dummy': ['Follows(person,person)',
                 'Loves(person,person)',
@@ -307,6 +307,13 @@ bk = {
               'Haswordauthor(author,word)',
               'Haswordtitle(title,word)',
               'Haswordvenue(venue,word)'],
+      'webkb': ['Link(page,page)',
+                'Has(word,page)',
+                'Instructorsof(page,page)',
+                'Hasanchor(word,page)',
+                'Membersofproject(page,page)',
+                'Departmentof(page,page)',
+                'Pageclass(page,class)'],
       'twitter': [#'Accounttype(account,+type)',
                   'Accounttype(account,type)',
                   #'Tweets(account,+word)',
@@ -367,7 +374,7 @@ bk = {
             'Educationdegree(person,educationdegree)',
             'Locale(person,locale)']
         }
-        
+
 experiments = [
             #{'id': '0', 'source':'dummy', 'target':'twitter', 'predicate':'proteinclass', 'to_predicate':'Accounttype'},
             #{'id': '15', 'source':'yeast', 'target':'twitter', 'predicate':'proteinclass', 'to_predicate':'Accounttype'},
@@ -421,28 +428,29 @@ experiments = [
             ##{'id': '48', 'source':'twitter', 'target':'facebook', 'predicate':'follows', 'to_predicate':'Edge'},
             ##{'id': '49', 'source':'imdb', 'target':'facebook', 'predicate':'workedunder', 'to_predicate':'Edge'},
             ##{'id': '50', 'source':'uwcse', 'target':'facebook', 'predicate':'advisedby', 'to_predicate':'Edge'},
-            {'id': '54', 'source':'cora', 'target':'imdb', 'predicate':'sametitle', 'to_predicate':'workedunder'},
+            #{'id': '54', 'source':'cora', 'target':'imdb', 'predicate':'sametitle', 'to_predicate':'workedunder'},
+            {'id': '116_un', 'source':'uwcse', 'target':'webkb', 'predicate':'advisedby', 'to_predicate':'departmentof'},
             ]
 
 firstRun = False
 n_runs = 1
 folds = n_folds = 3
-            
+
 if os.path.isfile('experiments/transfer_experiment.json'):
     with open('experiments/transfer_experiment.json', 'r') as fp:
         results = json.load(fp)
 else:
     results = { 'save': { }}
     firstRun = True
-        
+
 if firstRun:
     results['save'] = {
         'experiment': 0,
         'n_runs': 0,
         'seed': 441773,
-        'folds' : 3,      
+        'folds' : 3,
         }
-    
+
 while results['save']['n_runs'] < n_runs:
     experiment = results['save']['experiment'] % len(experiments)
     experiment_title = experiments[experiment]['id'] + '_' + experiments[experiment]['source'] + '_' + experiments[experiment]['target']
